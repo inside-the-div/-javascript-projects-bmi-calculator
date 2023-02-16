e5825a4e0b7581f508a4779307f739d3897bd7b1
kamrul_hasan_10 <rkamrulhasan7@gmail.com>
2023-02-16 23:45:24 +0600

    fix common file and js code refactor

--- js/bmi-calculator-js.js
+++ js/bmi-calculator-js.js
@@ -1,46 +1,25 @@
-document.getElementById("OutputInfo").style = "display: flex";
-document.getElementById("OutputResult").style = "display: none";
+_cmnHideElement("OutputResult");
 document.getElementById("heightUnit").value = "Centimeter";
 document.getElementById("weightUnit").value = "Kg";
 
 function BmiCalculatorFormValidate()
 {
     RemoveAllErrorMessage();
+
     var bmiHeight = document.getElementById("bmiHeight").value;
     var bmiWeight = document.getElementById("bmiWeight").value;
-    if(IsInputFieldEmpty("bmiHeight"))
-    {
-        ShowErrorMessageBottomOfTheInputFiled("bmiHeight", "Enter height.");
-        return false;
-    }
-    else if(isNaN(bmiHeight))
+
+    if(IsInputFieldEmpty("bmiHeight") || (isNaN(bmiHeight) && bmiHeight <= 0))
     {
         ShowErrorMessageBottomOfTheInputFiled("bmiHeight", "Enter valid height.");
         return false;
     }
     
-    if(parseFloat(bmiHeight) < 1)
+    if(IsInputFieldEmpty("bmiWeight") || (isNaN(bmiWeight) && bmiWeight <= 0))
     {
-        ShowErrorMessageBottomOfTheInputFiled("bmiHeight", "Enter valid height.");
-        return false;
-    }
-    
-    if(IsInputFieldEmpty("bmiWeight"))
-    {
-        ShowErrorMessageBottomOfTheInputFiled("bmiWeight", "Enter weight.");
+        ShowErrorMessageBottomOfTheInputFiled("bmiWeight", "Enter valid weight.");
         return false;
     }   
-    else if(isNaN(bmiWeight))
-    {
-        ShowErrorMessageBottomOfTheInputFiled("bmiWeight", "Enter valid weight.");
-        return false;
-    }
-    
-    if((parseFloat(bmiWeight)) < 1)
-    {
-        ShowErrorMessageBottomOfTheInputFiled("bmiWeight", "Enter valid weight.");
-        return false;
-    }
 
     return true;
 }
@@ -49,29 +28,24 @@ function BmiCalculatorReset()
 {
     document.getElementById("bmiHeight").value = "";
     document.getElementById("bmiWeight").value = "";
-    document.getElementById("bmiResult").innerHTML = "0.0";
     document.getElementById("heightUnit").value = "Centimeter";
     document.getElementById("weightUnit").value = "Kg";
+   
     RemoveAllErrorMessage();
+
+    _cmnHideElement("OutputResult");
+    _cmnShowElement("OutputInfo", "flex");
 }
 
 function BmiCalculation()
 {
     if(BmiCalculatorFormValidate())
     {
-        var count = 0;
         var result;
         var Height = Number(document.getElementById("bmiHeight").value)
         var Weight = Number(document.getElementById("bmiWeight").value)
         var heightUnit = document.getElementById("heightUnit").value;
         var weightUnit = document.getElementById("weightUnit").value;
-
-        if(count == 0)
-        {
-            document.getElementById("OutputInfo").style = "display: none";
-            document.getElementById("OutputResult").style = "display: flex";
-            count++;
-        }
 
         if(heightUnit == "Feet")
         {
@@ -93,6 +67,10 @@ function BmiCalculation()
 
         result = (Weight / (Height * Height));
 
-        document.getElementById("bmiResult").innerHTML = Number(result).toFixed(1);
+        document.getElementById("bmiResult").innerHTML = result.toFixed(1);
+
+        //result div show
+        _cmnHideElement("OutputInfo");
+        _cmnShowElement("OutputResult", "flex");
     }
 }
